796ea5e8ef90636b35d95aa8b029a9932a710c8e
hagin81 <34249812+hagin81@users.noreply.github.com>
2018-05-29 03:11:04 -0500

    Merged updated copy from gloria-project2 to master branch

---
+++ app.py
@@ -0,0 +1,55 @@
+# dependencies
+from flask import (Flask, render_template, jsonify, url_for )
+import pandas as pd 
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import Column, Integer, String, Numeric, Text, Float, Date
+import os
+import json
+
+
+# initialize database
+db_uri = os.getenv("DATABASE_URI", "sqlite:///project2.sqlite") 
+engine = create_engine( db_uri )
+
+# read data from project 2 and save to sqlite
+gh = pd.read_csv('DataSets/merged.csv')
+
+
+# save to database
+gh.to_sql('income', engine, if_exists='replace') 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
+    # query data from the database return it as json
+    conn = engine.connect() 
+    query = 'select * from income'
+    results = conn.execute( query ).fetchall() 
+    data = []
+    for i in results:
+        data.append({
+             "County": i['County'],
+             "Graduation": i['Graduation'],
+             "Income": str(i['Income']).replace("$","").replace(",",""),
+             "SAT": i['SAT']
+             })
+
+    
+    print( data )
+
+   
+    return jsonify( data )
+
+
+if __name__ == "__main__": 
+    app.run(debug=True)
+
+
+    
